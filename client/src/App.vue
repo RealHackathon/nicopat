8f1e214ffcb4805a2b3e5c318c047366d9f25e29
Nicolas Juin <juin.nicolas@gmail.com>
2018-04-14 16:18:51 +0200

    first bot messages

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,33 +9,45 @@
                 <div class="col-xs-12" id="waves"></div>
 
             </div>
-            <transition-group name="list">
+            <div class="responses-container">
 
-            <div class="row responses" v-for="(resp, index) in responses" :key="index">
+                <transition-group name="list" tag>
 
-                <div class="col-xs-6 response-term">
-                    <div> {{resp.term}} </div>
-                </div>
-                <div class="col-xs-6">
+                    <div class="row responses" v-for="(resp, index) in computedResponses" :key="index">
 
-                    <blockquote>
-                            <template v-for="line in pickASongLines(resp.songs)">
-                                {{line}} <br/>
-                            </template>
+                        <div class="col-xs-6 response-term">
+                            <div> {{resp.term}}</div>
+                        </div>
+                        <div class="col-xs-6">
 
+                            <blockquote>
+                                <template v-for="line in resp.song.Lines">
+                                    {{line}} <br/>
+                                </template>
 
-                    </blockquote>
+                                <cite> {{ resp.song.Artist }} - {{resp.song.Song}} </cite>
 
-                </div>
-                </div>
-            </transition-group>
+                            </blockquote>
 
+                        </div>
+                    </div>
+                </transition-group>
+
+            </div>
 
             <div class="row">
-                <div class="col-xs-12  " >
+                <div class="col-xs-12  ">
                     <div class="bot-message">
-                    <img src="/lyri-micro.png" alt="">
-                    </div> </div>
+                        <img src="/lyri-micro.png" class="pull-left">
+                        <p>{{botMessage}}
+                            <span v-for="(sugg, index) in suggestions" :key="index">
+                               <span class="sugg" @click="addSuggestion(sugg)"> {{sugg.Artist}} </span>
+                                <span v-if="index < suggestions.length-1"> ou </span>
+                            </span>
+
+                        </p>
+                    </div>
+                </div>
                 <div class="col-xs-12 input-container">
                     <input type="text" v-model="userInput" @keyup.enter="doSearch">
 
@@ -53,72 +65,127 @@
     import axios from "axios";
     import Logo from './Logo';
     import SiriWave from 'siriwavejs';
+    import {sample, sampleSize, uniqBy, last} from 'lodash';
+    import MSG from './messages';
 
     export default {
-        components:{ Logo },
+        components: {Logo},
         name: "app",
         data() {
             return {
                 userInput: "",
-                responses: [],// term / songs,
+                responses: [],// term / song,
                 lines: {},
-                isSearching:false
+                isSearching: false,
+                botMessage: '',
+                suggestions: []
             };
         },
         methods: {
             doSearch() {
                 var userInput = this.userInput;
-            this.isSearching = true;
+                this.suggestions = [];
+                if (!userInput.trim()) return;
+                this.isSearching = true;
+                this.botMessage = this.getRandMsg('searching');
+
                 axios.get("/api/songs/" + this.userInput).then(res => {
                     this.addToResponses(userInput, res.data);
+
                     this.isSearching = false;
-                    console.log(this.isSearching);
+                }).catch(err => {
+
+                    this.botMessage = this.getRandMsg('nothing');
+                    this.isSearching = false;
+
                 });
                 this.userInput = '';
             },
             addToResponses(term, songs) {
-                console.log(term,songs);
-                this.responses.push({term, songs});
+                if (!songs.length) {
+                    this.botMessage = this.getRandMsg('nothing');
+
+                    return;
+
+                }
+                var song = songs[0];
+                var msg = `J'ai trouvé du ${song.Artist} `;
+                if (songs.length > 1) {
+                    msg += this.getRandMsg('suggestions');
+                    var sample = uniqBy(songs, 'Artist');
+
+                    this.suggestions = sampleSize(sample.slice(1, sample.length - 1), 3);
+                }
+                else {
+                    msg += this.getRandMsg('nosuggestions');
+
+                }
+                this.botMessage = msg;
+                this.responses.push({term, song});
             },
-            pickASongLines(songs){
-                if(!songs[0]) return {};
+            pickASongLines(songs) {
+                if (!songs[0]) return {};
                 return songs[0].Lines;
+            },
+            getRandMsg(type) {
+                return sample(MSG[type]);
+            },
+            addSuggestion(song) {
+                var term = last(this.responses).term;
+                this.responses.push({term, song});
+
             }
+
         },
-        mounted(){
+        mounted() {
+            this.botMessage = this.getRandMsg('welcome');
             this.siriWave = new SiriWave({
-                container : document.getElementById('waves'),
-                width:600,
-                height:100,
-                color:'#9e00ba',
-                amplitude:0,
-                speed:0.05,
+                container: document.getElementById('waves'),
+                width: 600,
+                height: 100,
+                color: '#9e00ba',
+                amplitude: 0,
+                speed: 0.05,
             });
             this.siriWave.start();
         },
-        watch:{
-            isSearching:function(val){
-                if(val)
+        watch: {
+            isSearching: function (val) {
+                if (val)
                     this.siriWave.setAmplitude(0.3);
                 else
                     this.siriWave.setAmplitude(0.01);
             }
-        }
+        },
+        computed: {
+            computedResponses() {
+                var endI = this.responses.length;
+                var startI = Math.max(0, endI - 3);
+                var result = [];
+                for (var i = startI; i < endI; i++) {
+                    result.push(this.responses[i]);
+                }
+                return result;
+            }
+
+        },
     };
 </script>
 
 <style>
-    #app{
+    #app {
         font-family: 'Raleway', sans-serif;
     }
-    .logo > img{
+
+    .logo > img {
         max-width: 300px;
     }
-    .logo{
-        margin-top: 50px;
+
+    .logo {
+        margin-top: 20px;
     }
 
-    .input-container > input{
+    .input-container > input {
         display: block;
         margin: 0 auto;
         width: 100%;
@@ -129,24 +196,28 @@
         line-height: 1.3333333;
         border-radius: 5px;
         border: 1px solid #9e00ba;
-
     }
 
     .list-item {
         display: inline-block;
         margin-right: 10px;
     }
+
     .list-enter-active, .list-leave-active {
         transition: all 1s;
     }
-    .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+
+    .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */
+    {
         opacity: 0;
         transform: translateY(30px);
     }
-    .responses{
+
+    .responses {
         position: relative;
     }
-    .response-term{
+
+    .response-term {
         text-align: right;
         opacity: 0.7;
         /*position: absolute;*/
@@ -154,15 +225,34 @@
         transform: translateY(170%);
     }
 
-    #waves{
+    #waves {
         text-align: center;
     }
 
-    .bot-message{
+    .bot-message {
         max-width: 600px;
         margin: 0 auto;
     }
 
+    .bot-message > p {
+        padding-top: 45px;
+    }
+
+    cite {
+        font-size: 0.8em;
+        opacity: 0.8;
+        text-align: right;
+    }
+
+    .responses-container {
+        max-height: 50vh;
+
+    }
+
+    span.sugg {
+        color: #9e00ba;
+        cursor: pointer;
+    }
 
 
 </style>
---
+++ client/src/messages.js
@@ -0,0 +1,10 @@
+var messages = {
+        welcome: ['Salut, je suis Lyri. Je réponds en chansons.'],
+        nothing: ["Ooops, j'ai rien trouvé. Je suis le produit d'un hackaton, tu crois quoi ?!"],
+        suggestions: ["sinon j'ai du", "ou si tu préfères du"],
+        nosuggestions: ["et... bah voilà.", "et pas grand chose d'autre.", "et puis c'est tout.", "et c'est mon dernier mot."],
+        searching: ["Je regarde et je reviens.", "Humm...."]
+    }
+;
+
+export default messages;
